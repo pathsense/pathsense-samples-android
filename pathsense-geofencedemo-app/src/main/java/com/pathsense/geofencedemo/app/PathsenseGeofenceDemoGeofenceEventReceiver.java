27d01ac0e2a45d76d5e4c69073eee702f8e14e77
dyerjo <john@pathsense.com>
2019-10-20 15:16:09 -0700

    Update targetSdkVersion to 28; migrate to AndroidX

--- pathsense-geofencedemo-app/src/main/java/com/pathsense/geofencedemo/app/PathsenseGeofenceDemoGeofenceEventReceiver.java
+++ pathsense-geofencedemo-app/src/main/java/com/pathsense/geofencedemo/app/PathsenseGeofenceDemoGeofenceEventReceiver.java
@@ -3,41 +3,41 @@
  */
 
 package com.pathsense.geofencedemo.app;
+
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.pathsense.android.sdk.location.PathsenseGeofenceEvent;
 import com.pathsense.android.sdk.location.PathsenseGeofenceEventReceiver;
-public class PathsenseGeofenceDemoGeofenceEventReceiver extends PathsenseGeofenceEventReceiver
-{
-	static final String TAG = PathsenseGeofenceDemoGeofenceEventReceiver.class.getName();
-	//
-	@Override
-	protected void onGeofenceEvent(Context context, PathsenseGeofenceEvent geofenceEvent)
-	{
-		Log.i(TAG, "geofence = " + geofenceEvent.getGeofenceId() + ", " + geofenceEvent.getLatitude() + ", " + geofenceEvent.getLongitude() + ", " + geofenceEvent.getRadius());
-		//
-		if (geofenceEvent.isEgress())
-		{
-			Location location = geofenceEvent.getLocation();
-			Log.i(TAG, "geofenceEgress = " + location.getTime() + ", " + location.getProvider() + ", " + location.getLatitude() + ", " + location.getLongitude() + ", " + location.getAltitude() + ", " + location.getSpeed() + ", " + location.getBearing() + ", " + location.getAccuracy());
-			SoundManager.getInstance(context).playDing();
-			// broadcast event
-			Intent geofenceEventIntent = new Intent("geofenceEvent");
-			geofenceEventIntent.putExtra("geofenceEvent", geofenceEvent);
-			LocalBroadcastManager.getInstance(context).sendBroadcast(geofenceEventIntent);
-		} else if (geofenceEvent.isIngress())
-		{
-			Location location = geofenceEvent.getLocation();
-			Log.i(TAG, "geofenceIngress = " + location.getTime() + ", " + location.getProvider() + ", " + location.getLatitude() + ", " + location.getLongitude() + ", " + location.getAltitude() + ", " + location.getSpeed() + ", " + location.getBearing() + ", " + location.getAccuracy());
-			SoundManager.getInstance(context).playJingle();
-			// broadcast event
-			Intent geofenceEventIntent = new Intent("geofenceEvent");
-			geofenceEventIntent.putExtra("geofenceEvent", geofenceEvent);
-			LocalBroadcastManager.getInstance(context).sendBroadcast(geofenceEventIntent);
-		}
-	}
+
+public class PathsenseGeofenceDemoGeofenceEventReceiver extends PathsenseGeofenceEventReceiver {
+    static final String TAG = PathsenseGeofenceDemoGeofenceEventReceiver.class.getName();
+
+    //
+    @Override
+    protected void onGeofenceEvent(Context context, PathsenseGeofenceEvent geofenceEvent) {
+        Log.i(TAG, "geofence = " + geofenceEvent.getGeofenceId() + ", " + geofenceEvent.getLatitude() + ", " + geofenceEvent.getLongitude() + ", " + geofenceEvent.getRadius());
+        //
+        if (geofenceEvent.isEgress()) {
+            Location location = geofenceEvent.getLocation();
+            Log.i(TAG, "geofenceEgress = " + location.getTime() + ", " + location.getProvider() + ", " + location.getLatitude() + ", " + location.getLongitude() + ", " + location.getAltitude() + ", " + location.getSpeed() + ", " + location.getBearing() + ", " + location.getAccuracy());
+            SoundManager.getInstance(context).playDing();
+            // broadcast event
+            Intent geofenceEventIntent = new Intent("geofenceEvent");
+            geofenceEventIntent.putExtra("geofenceEvent", geofenceEvent);
+            LocalBroadcastManager.getInstance(context).sendBroadcast(geofenceEventIntent);
+        } else if (geofenceEvent.isIngress()) {
+            Location location = geofenceEvent.getLocation();
+            Log.i(TAG, "geofenceIngress = " + location.getTime() + ", " + location.getProvider() + ", " + location.getLatitude() + ", " + location.getLongitude() + ", " + location.getAltitude() + ", " + location.getSpeed() + ", " + location.getBearing() + ", " + location.getAccuracy());
+            SoundManager.getInstance(context).playJingle();
+            // broadcast event
+            Intent geofenceEventIntent = new Intent("geofenceEvent");
+            geofenceEventIntent.putExtra("geofenceEvent", geofenceEvent);
+            LocalBroadcastManager.getInstance(context).sendBroadcast(geofenceEventIntent);
+        }
+    }
 }
